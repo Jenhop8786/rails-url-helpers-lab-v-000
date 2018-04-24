03067f818fdb0abec578e7df9f381d0a53ac4bf6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-24 22:02:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
       @student = Student.find(params[:id])
     end
 
-    def activate 
+    def activate
       @student = Student.find(params[:id])
       @student.active =!@student.active
       @student.save
